41a45d6ab08780195c5882461fea4009b042835c
Ariel022234 <ariel022234@gmail.com>
2026-08-29 15:57:56 -0300

    otra version

--- src/cuadrado/forma/Punto.java
+++ src/cuadrado/forma/Punto.java
@@ -6,14 +6,13 @@ public class Punto {
 	
 	public Punto(float x, float y) {
 		super();
-		this.setXY(x, y);
-		
+		this.x = x;
+		this.y = y;
 	}
 	
 	public void setXY(float x, float y) {
 		this.setX(x);
 		this.setY(y);
-		
 	}
 
 	public float getX() {
